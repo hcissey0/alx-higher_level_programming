75b1a4595544085c781d14e61053fa23ec1b46a4
Hassan Cissey <hcissey0@gmail.com>
2023-06-08 16:34:19 +0000

    100-my_calculator.py: My simple calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import sys
-    from calculator_1 import add, sub, mul, div
-    if not len(sys.argv) == 4:
+import sys
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if sys.argv[2][0] != '+' and sys.argv[2][0] != '-'\
-            and sys.argv[2][0] != '*' and sys.argv[2][0] != '/':
+
+    operator = sys.argv[2]
+
+    if operator not in "+-*/":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    print("{} {} {} = ".format(sys.argv[1], sys.argv[2], sys.argv[3]), end="")
-    match sys.argv[2]:
-        case "+":
-            print(add(int(sys.argv[1]), int(sys.argv[3])))
-        case "-":
-            print(sub(int(sys.argv[1]), int(sys.argv[3])))
-        case "*":
-            print(mul(int(sys.argv[1]), int(sys.argv[3])))
-        case "/":
-            print(div(int(sys.argv[1]), int(sys.argv[3])))
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    print("{} {} {} = ".format(a, operator, b), end="")
+
+    if operator == "+":
+        print(add(a, b))
+    elif operator == "-":
+        print(sub(a, b))
+    elif operator == "*":
+        print(mul(a, b))
+    elif operator == "/":
+        print(div(a, b))
+
+
+if __name__ == "__main__":
+    main()
