e52d5356c8e51bdcfe885bdce8caed562c2899af
Hassan Cissey <hcissey0@gmail.com>
2023-06-09 05:15:33 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == '__main__':
-    for name in dir(__import__("hidden_4.pyc")):
+    names = dir(hidden_4)
+    for name in sorted(names):
         if not name.startswith("__"):
             print(name)
