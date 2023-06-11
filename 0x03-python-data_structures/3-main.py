6505247d05eeeb4442e160eaaa83decf224b773e
Hassan Cissey <hcissey0@gmail.com>
2023-06-11 03:04:18 +0000

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = [i for i in range(500)]
+my_list = [i for i in range(20) if i % 2 == 0]
 print_reversed_list_integer(my_list)
