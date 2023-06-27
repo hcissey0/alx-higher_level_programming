3a9ad62c6345e0a2db8c0bcd624a74946cdbe204
Hassan Cissey <hcissey0@gmail.com>
2023-06-27 15:23:22 +0000

    6square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -72,12 +72,14 @@ class Square:
         """Prints out the square with #
         """
         x, y = self.__position
-        if self.size == 0:
+        if self.__size == 0:
             print()
         else:
-            for i in range(y):
+            for i in range(self.__position[1]):
                 print()
-            for i in range(self.size):
-                for j in range(self.size):
-                    print(" " * x + "#" * self.size)
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(" ", end="")
+                for k in range(self.__size):
+                    print("#", end="")
                 print()
