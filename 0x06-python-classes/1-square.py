7ef68b71467199a537e538412cb8a5676ac3fe92
Hassan Cissey <hcissey0@gmail.com>
2023-06-27 10:58:09 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """This is a square Class
+    """
+    def __init__(self, size):
+        """This is the constructor
+        Args:
+            size: the size of the square
+        """
+        self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+class Square:
+    """This is a square Class
+    """
+    def __init__(self, size=0):
+        """This is the constructor
+
+        Args:
+            size: the size of the square
+
+        Raises:
+            TypeError: when size is not an int
+            ValueError: when size is < 0
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        
+    def area(self):
+        return self.__size * self.__size
