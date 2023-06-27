0aea67ee513b637165f885db7a697b82cb65067b
Hassan Cissey <hcissey0@gmail.com>
2023-06-27 05:05:41 +0000

    magic calculation

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -3,10 +3,15 @@
 
 /**
  * print_python_list - prints python list
- * @list: the list
+ * @p: the list
  */
 void print_python_list(PyObject *p)
 {
+	PyObject *item;
+	Py_ssize_t size;
+	Py_ssize_t allocated;
+	Py_ssize_t i;
+
 	setbuf(stdout, NULL);
 
 	if (!PyList_Check(p))
@@ -15,16 +20,16 @@ void print_python_list(PyObject *p)
 		return;
 	}
 
-	Py_ssize_t size = PyList_GET_SIZE(p);
-	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+	size = PyList_GET_SIZE(p);
+	allocated = ((PyListObject *)p)->allocated;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %zd\n", size);
 	printf("[*] Allocated = %zd\n", allocated);
 
-	for (Py_ssize_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		PyObject *item = PyList_GET_ITEM(p, i);
+		item = PyList_GET_ITEM(p, i);
 		printf("Element %zd: %s\n", i, item->ob_type->tp_name);
 
 		if (PyBytes_Check(item))
@@ -35,11 +40,14 @@ void print_python_list(PyObject *p)
 }
 
 /**
- * print_pyton_bytes - print spython byte
+ * print_python_bytes - print spython byte
  * @p: the object
  */
 void print_python_bytes(PyObject *p)
 {
+	char *string;
+	Py_ssize_t size;
+
 	setbuf(stdout, NULL);
 
 	if (!PyBytes_Check(p))
@@ -48,8 +56,8 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	Py_ssize_t size = PyBytes_Size(p);
-	char *string = PyBytes_AsString(p);
+	size = PyBytes_Size(p);
+	string = PyBytes_AsString(p);
 
 	printf("[.] bytes object info\n");
 	printf("  size: %zd\n", size);
@@ -71,15 +79,16 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_float(PyObject *p)
 {
-	setbuf(stdout, NULL);
+	double value;
 
+	setbuf(stdout, NULL);
 	if (!PyFloat_Check(p))
 	{
 		printf("[ERROR] Invalid Float Object\n");
 		return;
 	}
 
-	double value = PyFloat_AS_DOUBLE(p);
+	value = PyFloat_AS_DOUBLE(p);
 
 	printf("[.] float object info\n");
 	printf("  value: %.15g\n", value);
