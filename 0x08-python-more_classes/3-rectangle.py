0ccc9088748a42a090c88082ed4b428ea4903e11
Hassan Cissey <hcissey0@gmail.com>
2023-07-04 00:27:20 +0000

    Perimeter and Area

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -43,4 +43,4 @@ class Rectangle:
         """Calculates the perimeter of the rectangle"""
         if self.width == 0 or self.height == 0:
             return 0
-        return 2 * (self.width + self.height)
+        return self.width + self.height
