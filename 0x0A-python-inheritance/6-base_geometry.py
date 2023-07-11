c7e503888dcf1e40a730dd5d84500123b6e1728e
Hassan Cissey <hcissey0@gmail.com>
2023-07-11 05:59:18 +0000

    add attrubute

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -4,7 +4,7 @@
 
 class BaseGeometry():
     """This class is a geometry class"""
-    
+
     def area(self):
         """finds the area of the class"""
         raise Exception("area() is not implemented")
