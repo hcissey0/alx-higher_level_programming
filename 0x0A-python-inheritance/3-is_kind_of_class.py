5dd1bd2261194e3956d2e8d732f8351b68fd398b
Hassan Cissey <hcissey0@gmail.com>
2023-07-11 04:45:01 +0000

    kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module has one function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ckeck if an obj is instance of a class"""
+    return isinstance(obj, a_class)
