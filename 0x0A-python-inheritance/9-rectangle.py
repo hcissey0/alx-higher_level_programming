6aea1bf99e1af24565a6357357ac8381863f4a08
Hassan Cissey <hcissey0@gmail.com>
2023-07-11 05:39:14 +0000

    rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module has 2 classes"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """ this is a rectangle class"""
+
+    def __init__(self, width, height):
+        """the initializor func"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """the string rep"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """REctangle: area of the rectangle"""
+        return self.__width * self.__height
