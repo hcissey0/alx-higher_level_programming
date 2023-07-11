3cbc646a130383f43a949bfbd6a10d211926df17
Hassan Cissey <hcissey0@gmail.com>
2023-07-11 04:48:40 +0000

    inherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module has one function"""
+
+
+def inherits_from(obj, a_class):
+    """checks inheritance of an obj"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
