a08e56d71cf0c06eb02e19215f2ff2794ea3866c
Hassan Cissey <hcissey0@gmail.com>
2023-07-12 03:12:27 +0000

    Class To JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module has only one function"""
+
+
+def class_to_json(obj):
+    """This function converts a class to a json object"""
+    import json
+    at = {k: v for k, v in obj.__dict__.items() if not k.startswith("__")}
+    return at
