a006928746881740ccb3dc664c055e5e2dfc3b23
Hassan Cissey <hcissey0@gmail.com>
2023-07-12 04:34:41 +0000

    Log parsing

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This module Parses logs"""
+
+
+import sys
+
+
+c = 0
+tsize = 0
+scodes = {200: 0,
+          301: 0,
+          400: 0,
+          401: 0,
+          403: 0,
+          404: 0,
+          405: 0,
+          500: 0
+          }
+try:
+    for l in sys.stdin:
+        tokens = l.strip().split()
+        if len(tokens) > 2:
+            scode = int(tokens[-2])
+            fsize = int(tokens[-1])
+            tsize += fsize
+            if scode in scodes:
+                scodes[scode] += 1
+        c += 1
+        if c % 10 == 0:
+            print("File size: {}".format(tsize))
+            for cd, num in sorted(scodes.items()):
+                if num > 0:
+                    print("{}: {}".format(cd, num))
+except KeyboardInterrupt:
+    pass
+print("File size: {}".format(tsize))
+for cd, num in sorted(scodes.items()):
+    if num > 0:
+        print("{}: {}".format(cd, num))
