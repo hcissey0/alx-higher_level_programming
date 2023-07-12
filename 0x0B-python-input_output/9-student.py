ebccbfecb9e342957a453c8502915e43439e3f40
Hassan Cissey <hcissey0@gmail.com>
2023-07-12 03:27:25 +0000

    Student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains a class"""
+
+
+class Student():
+    """This is a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """The initializor of the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns a dict repr of a Student"""
+        import json
+        return {k: v for k, v in self.__dict__.items() if not k.startswith("__")}
