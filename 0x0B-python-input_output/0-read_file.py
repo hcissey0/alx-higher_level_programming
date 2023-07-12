e08504aaadecec3d0437a0421e298a849f101f24
Hassan Cissey <hcissey0@gmail.com>
2023-07-12 01:55:50 +0000

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""This module contains a function that prints
+a file content"""
+
+
 def read_file(filename=""):
     """This function prints a file content"""
     if filename is None or type(filename) is not str:
