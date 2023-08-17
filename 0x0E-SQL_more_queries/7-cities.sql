6771b2cac5581cac5e41709fcee0551dc88fed34
Hassan Cissey <hcissey0@gmail.com>
2023-08-17 02:57:51 +0000

    states

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,6 @@
+-- Creates a database and a table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL FOREIGN KEY
