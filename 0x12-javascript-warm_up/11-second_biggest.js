2f148733d9f4df85e939e218e3c46d6b698266d7
Hassan Cissey <hcissey0@gmail.com>
2023-09-11 18:22:07 +0000

    Second Biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-let sb = 0;
-let b = 0;
-const arg = process.argv.slice(2);
-for (let i = 0; i < arg.length; i++) {
-  if (Number(arg[i]) > b) {
-    sb = b;
-    b = Number(arg[i]);
-  }
+const arg = process.argv.slice(2).map(Number);
+if (arg.length <= 1) {
+  console.log(0);
+} else {
+  arg.sort((a, b) => b - a);
+  console.log(arg[1]);
 }
-console.log(sb);
