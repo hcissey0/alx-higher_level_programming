527ef328806dc7d89a4bb74f0e8233cf9e573f5f
Hassan Cissey <hcissey0@gmail.com>
2023-09-11 18:04:58 +0000

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const size = Number(process.argv.slice(2)[0]);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
