a327b56519bffa23a9f4465c82d786c0191d89d2
Hassan Cissey <hcissey0@gmail.com>
2023-09-29 12:21:22 +0000

    Post Email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,6 +8,9 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    url = url + "?email=" + email
-    with urllib.request.urlopen(url) as res:
+    values = {'email': email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as res:
         print(res.read().decode('utf-8'))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,9 +2,10 @@
 """This script takes a url and sends a request to the url"""
 
 import urllib.request
+import urllib.error
 import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    
+    urllib.
