946600e3d87926b670837492881218c3449ed7c5
Hassan Cissey <hcissey0@gmail.com>
2023-09-29 02:14:13 +0000

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is the algorithm to find the peak in a list"""
+
+
+def find_peak(list_of_integers):
+    """This function is used to find the peak"""
+    if not list_of_integers:
+        return None
+    return max(list_of_integers)
