89e101c5f5d344347e8061a6e788a228f02c4cd7
Hassan Cissey <hcissey0@gmail.com>
2023-09-29 03:07:04 +0000

    Catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script is used to do catch me if you can
-curl -s -X PUT "You got me!" -w "%{stdout}" "0.0.0.0:5000/catch_me"
+curl -s -X PUT -d "You got me!" http://0.0.0.0:5000/catch_me
