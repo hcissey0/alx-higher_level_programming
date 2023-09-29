6ad6e35893fecb5229dde2bcbb6c392bdd326eee
Hassan Cissey <hcissey0@gmail.com>
2023-09-29 12:14:46 +0000

    Post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,16 +2,12 @@
 """This scrip is used to send a post request with email as the parameter"""
 
 import urllib.request
-import urllib.parse
 import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    values = {'email': email}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as res:
+    url = url + "?email=" + email
+    with urllib.request.urlopen(url) as res:
         print(res.read().decode('utf-8')
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""This script takes a url and sends a request to the url"""
+
+import urllib
