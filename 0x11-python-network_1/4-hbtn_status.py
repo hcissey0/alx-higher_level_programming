c73ac1840f8ccecc114040fb01f521c26bbed9af
Hassan Cissey <hcissey0@gmail.com>
2023-09-29 12:42:29 +0000

    response header value

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.htbn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     res = requests.get(url)
     print("Body response:")
     print(f"\t- type: {type(res.text)}")
