b193844d34df116dee0234ecc32fe03d5c8f2480
Hassan Cissey <hcissey0@gmail.com>
2023-09-29 01:24:30 +0000

    Curl to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script uses GET to chek the body of a page that returns 200
-curl -s -o /dev/null -w "%{http_code}%" "$1" | grep -q "200" && curl -s "$1"
+curl -s -o /dev/null -w "%{http_code}%" "$1" | grep "200" && curl -s "$1"
