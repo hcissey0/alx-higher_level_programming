6f5506e5a11f49f2929d999b5addd585adafe7ac
Hassan Cissey <hcissey0@gmail.com>
2023-09-29 12:36:23 +0000

    What's my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-"""
+"""This script is used to retch useing requests"""
+
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.htbn.io/status'
+    res = requests.get(url)
+    print("Body response:")
+    print(f"\t- type: {type(res.text)}")
+    print(f"\t- content: {res.text}")
