b84ba66c9174523d1110996cb0816308294c5b4f
Hassan Cissey <hcissey0@gmail.com>
2023-09-29 13:44:58 +0000

    Search Api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,13 +7,16 @@ import sys
 
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
-    q = sys.argv[1] if sys.argv[1] else ""
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
     payload = {'q': q}
     res = requests.post(url, data=payload)
     try:
         jsres = res.json()
         if jsres:
-            print(f"[{jsres.get(id)}] {jsres.get(name)}")
+            print(f"[{jsres.get('id')}] {jsres.get('name')}")
         else:
             print("No result")
     except ValueError:
