4e926ac0dd74436f53a185d9c3086b674841588b
Hassan Cissey <hcissey0@gmail.com>
2023-09-29 12:24:55 +0000

    Error Code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,4 +8,8 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    urllib.
+    req = urllib.request.Request(url)
+    try:
+        urllib.request.urlopen(req)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
