632bf5eebd36d867b8ae8aa7799fec16d619226e
Hassan Cissey <hcissey0@gmail.com>
2023-09-29 02:29:39 +0000

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,4 +6,5 @@ def find_peak(list_of_integers):
     """This function is used to find the peak"""
     if not list_of_integers:
         return None
-    return sorted(list_of_integers)[-1]
+    list_of_integers.sort()
+    return list_of_integers[-1]
