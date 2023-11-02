fb6a40919e96f58ce0986e8cf9d740be3266b2ee
Hassan Cissey <hcissey0@gmail.com>
2023-11-02 04:31:42 +0000

    JQuery -- StarWars

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,2 +1,7 @@
-$(document).ready(function() {
-	$
+$(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    for (let i = 0; i < data.results.length; i++) {
+      $('UL#list_movies').append('<li>' + data.results[i].title + '</li>');
+    }
+  });
+});
